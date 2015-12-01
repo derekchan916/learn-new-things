e38489d678a2123c9023bec28bec79490a38861f
Derek Chan <derekchan916@gmail.com>
2015-11-30 23:26:05 -0500

    Try JSX/

---
+++ react-my-first-component/js/component.jsx
@@ -0,0 +1,39 @@
+var ClickCounter = React.createClass({
+  getInitialState: function(){
+    return {count: 0};
+  },
+
+  click: function(event){
+    event.preventDefault();
+    this.setState({count: this.state.count + 1});
+  },
+
+  render: function(){
+    return (
+      <div>
+        <button onClick={this.click}>CLICK ME</button>
+        <span>{this.state.count}</span>
+      </div>
+
+    // ex-JSX
+    // React.createElement('div', {},
+    //   React.createElement(
+    //     'button',
+    //     { onClick: this.click },
+    //     "CLICK ME"
+    //   ),
+    //   React.createElement('span', {}, this.state.count)
+    // )
+   );
+  }
+});
+
+ReactDOM.render(
+  React.createElement(ClickCounter, {}, ""),
+  document.getElementById('my-component')
+);
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
