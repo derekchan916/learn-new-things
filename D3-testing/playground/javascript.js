4a3aed3d7d3782ab4fd150b4094dcf11fe3eef3a
Derek Chan <derekchan916@gmail.com>
2015-12-02 18:53:10 -0500

    Add D3 function for CSV file.

--- D3-testing/playground/javascript.js
+++ D3-testing/playground/javascript.js
@@ -1,11 +1,13 @@
-var myArrayOfObjects = [
-  { x: 100, y: 100 },
-  { x: 130, y: 120 },
-  { x: 80, y: 180 },
-  { x: 180, y: 80 },
-  { x: 180, y: 40 }
-]
+// will have to get a real csv file
+d3.csv("data.csv", type, function(myArrayOfObjects) {
+  myArrayOfObjects.forEach(function (d) {
+    console.log(d.x + ", " + d.y);
+  });
 
-myArrayOfObjects.forEach(function (d) {
-  console.log(d.x + ", " + d.y);
+  function type (d) {
+    // parsing it into a number
+    d.x = +d.x;
+    d.y = +d.y;
+    return d;
+  }
 });
