06d1cb15eca3b39b963068c1e9cad1d72301f321
Derek Chan <derekchan916@gmail.com>
2015-12-02 18:45:09 -0500

    Change file name.

--- D3-testing/playground/javascript.js
+++ D3-testing/playground/javascript.js
@@ -1,1 +1,11 @@
-console.log("testing")
+var myArrayOfObjects = [
+  { x: 100, y: 100 },
+  { x: 130, y: 120 },
+  { x: 80, y: 180 },
+  { x: 180, y: 80 },
+  { x: 180, y: 40 }
+]
+
+myArrayOfObjects.forEach(function (d) {
+  console.log(d.x + ", " + d.y);
+});
