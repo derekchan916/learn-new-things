525d850ebccaa4cbabacc59258ee837b63d31eab
Derek Chan <derekchan916@gmail.com>
2015-12-03 22:26:24 -0500

    Append SVG.

--- D3-testing/bar-graph/bar.js
+++ D3-testing/bar-graph/bar.js
@@ -2,3 +2,10 @@ var outerWidth = 800;
 var outerHeight = 400;
 var margin = { left: 50, top: 50, right: 10, bottom: 50 };
 var barPadding = 0.05;
+
+var innerWidth = outerWidth - margin.left - margin.right;
+var innerHeight = outerHeight - margin.top - margin.bottom;
+
+var svg = d3.select("#chart").append("svg")
+  .attr("width", outerWidth)
+  .attr("height", outerHeight);
