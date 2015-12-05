462e41144e521fad386178765ebf8e6606704b29
Derek Chan <derekchan916@gmail.com>
2015-12-05 16:46:17 -0500

    Add googleSearch method.

--- python-testing/playground.py
+++ python-testing/playground.py
@@ -5,3 +5,15 @@ def getUserInput():
     user_input = raw_input("Some input please: ")
     if len(user_input) > 0:
         googleSearch(user_input)
+
+def googleSearch(search_query):
+    query = urllib.urlencode({'q': search_query})
+    url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
+    search_response = urllib.urlopen(url)
+    search_results = search_response.read()
+    results = json.loads(search_results)
+    data = results['responseData']
+    print data['results'][0]['titleNoFormatting'] + " from " +  data['results'][0]['visibleUrl']
+
+while True:
+    getUserInput()
