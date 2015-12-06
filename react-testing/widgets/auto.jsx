68cf684ad2bb8781fc0594ae97b580c1776cf708
Derek Chan <derekchan916@gmail.com>
2015-12-06 01:14:29 -0500

    Add input and initial state.

--- react-testing/widgets/auto.jsx
+++ react-testing/widgets/auto.jsx
@@ -1,6 +1,16 @@
 var React = require('react');
 
 var AutoComplete = React.createClass({
-
+  getInitialState: function() {
+    return { input: "" };
+  },
+  render: function() {
+    return (
+      <input>
+        {this.state.input}
+      </input>
+    )
+  }
 });
+
 module.exports = AutoComplete;
--- react-testing/widgets/bundle.js
+++ react-testing/widgets/bundle.js
@@ -19446,58 +19446,17 @@
 	  displayName: "AutoComplete",
 
 	  getInitialState: function () {
-	    return { inputVal: "" };
+	    return { input: "" };
 	  },
-
-	  handleInput: function (event) {
-	    this.setState({ inputVal: event.currentTarget.value });
-	  },
-
-	  matches: function () {
-	    var matches = [];
-	    if (this.state.inputVal.length === 0) {
-	      return this.props.names;
-	    }
-
-	    this.props.names.forEach((function (name) {
-	      var sub = name.slice(0, this.state.inputVal.length);
-	      if (sub.toLowerCase() === this.state.inputVal.toLowerCase()) {
-	        matches.push(name);
-	      }
-	    }).bind(this));
-
-	    if (matches.length === 0) {
-	      matches.push("No matches");
-	    }
-
-	    return matches;
-	  },
-
-	  selectName: function (event) {
-	    var name = event.currentTarget.innerText;
-	    this.setState({ inputVal: name });
-	  },
-
 	  render: function () {
-	    var results = this.matches();
 	    return React.createElement(
-	      "div",
+	      "input",
 	      null,
-	      React.createElement("input", { onChange: this.handleInput, value: this.state.inputVal }),
-	      React.createElement(
-	        "ul",
-	        null,
-	        results.map((function (result, i) {
-	          return React.createElement(
-	            "li",
-	            { key: i, onClick: this.selectName },
-	            result
-	          );
-	        }).bind(this))
-	      )
+	      this.state.input
 	    );
 	  }
 	});
+
 	module.exports = AutoComplete;
 
 /***/ }
