16889db781d5565d21b936e05052ea445f46b085
Derek Chan <derekchan916@gmail.com>
2015-12-06 20:36:41 -0500

    Complete function.

--- javascript-testing/cool-functions.js
+++ javascript-testing/cool-functions.js
@@ -1,7 +1,25 @@
-navigator.geolocation.getCurrentPosition(this.pollWeather);
-
 var pollWeather = function (location) {
-  
+  var lat = location.coords.latitude;
+  var long = location.coords.longitude;
+  this.area = [lat, long]
+  var url = "http://api.openweathermap.org/data/2.5/weather?";
+  var params = {
+    lat: location.coords.latitude,
+    lon: location.coords.longitude
+  };
+  url += toQueryString(params)
+  url += "&APPID=645c5d39c7603f17e23fcaffcea1a3c1"
+
+  var xmlhttp = new XMLHttpRequest();
+  var that = this;
+
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.status === 200 && xmlhttp.readyState === XMLHttpRequest.DONE) {
+      that.locationData = JSON.parse(xmlhttp.responseText);
+    }
+  }
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
 };
 
 var toQueryString = function (obj) {
@@ -13,3 +31,5 @@ var toQueryString = function (obj) {
   }
   return parts.join("&");
 };
+
+navigator.geolocation.getCurrentPosition(this.pollWeather);
