31e834237220359b61389a706ac0e72d498dd180
Derek Chan <derekchan916@gmail.com>
2015-12-07 17:44:45 -0500

    Add variable headers utilizing props.

--- react-testing/widgets/tabs.jsx
+++ react-testing/widgets/tabs.jsx
@@ -1,7 +1,13 @@
 var React = require('react');
 
 var Headers = React.createClass({
-
+  render: function() {
+    var selected = this.props.selectedPane;
+    var that = this;
+    var headers = this.props.panes.map(function (pane, index) {
+      
+    })
+  }
 });
 
 module.exports = Tabs;
