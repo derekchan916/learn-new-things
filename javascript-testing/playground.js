f8eb1f42723fc25b768f296c19ae0dd333aee766
Derek Chan <derekchan916@gmail.com>
2015-12-08 11:17:18 -0500

    Answer to difficult javascript question.

--- javascript-testing/playground.js
+++ javascript-testing/playground.js
@@ -5,5 +5,18 @@ for(var i = 0; i < 10; i++) {
   }
 }
 x[ 2 ]();
+// what does this produce, and how can we fix it without changing x[ 2 ]()
 
-s
+var x = [];
+for(var i = 0; i < 10; i++) {
+  var y = function(i) {
+    return i
+  };
+  x[i] = (function(i) {
+    return function() {
+      console.log("Value: " + y(i))
+    };
+  })(i);
+}
+
+x[ 2 ]();
