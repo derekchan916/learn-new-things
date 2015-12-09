86c478f8c8dbe0183f426eb917819ba9ba436c99
Derek Chan <derekchan916@gmail.com>
2015-12-09 17:23:43 -0500

    Fill in Todos controller for create, index, show, destroy, update.)

--- react-testing/todo-react-app/app/controllers/todos_controller.rb
+++ react-testing/todo-react-app/app/controllers/todos_controller.rb
@@ -1,3 +1,34 @@
 class TodosController < ApplicationController
-  
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save!
+    render json: @todo
+  end
+
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy!
+    render json: {}
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update!(todo_params)
+    render json: @todo
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :body, :boolean)
+  end
 end
