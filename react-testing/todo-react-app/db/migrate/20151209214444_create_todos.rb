937d4f8a31e88486324ab778658cd136c00fa007
Derek Chan <derekchan916@gmail.com>
2015-12-09 16:48:27 -0500

    Migrated Todo model.

--- react-testing/todo-react-app/db/migrate/20151209214444_create_todos.rb
+++ react-testing/todo-react-app/db/migrate/20151209214444_create_todos.rb
@@ -1,7 +1,9 @@
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
-
+      t.string :title, null: false
+      t.text :body, null: false
+      t.boolean :done, default: false
       t.timestamps null: false
     end
   end
