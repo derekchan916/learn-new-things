23bc0ff5a0c1494dd5942a798db6ad2befb2503a
Derek Chan <derekchan916@gmail.com>
2015-12-12 15:50:14 -0500

    Create render function with todos.title.

--- react-testing/todo-react-app/frontend/components/todo_list.jsx
+++ react-testing/todo-react-app/frontend/components/todo_list.jsx
@@ -4,5 +4,20 @@ var TodoStore = require('../stores/todo_store.js')
 var TodoList = React.createClass({
   getInitialState: function() {
     return ({ todos: TodoStore.all() });
+  },
+
+  render: function() {
+    var todos = this.state.todos;
+    return (
+      <div>
+        <ul>
+          {
+            todos.map(function(todo) {
+              return (<li> { todo.title }</li>)
+            })
+          }
+        </ul>
+      </div>
+    )
   }
 })
