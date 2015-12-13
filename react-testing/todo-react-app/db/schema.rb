cc970db483199200a05a75bab86bd0831433d1b1
Derek Chan <derekchan916@gmail.com>
2015-12-13 01:08:32 -0500

    Migrated Steps.

--- react-testing/todo-react-app/db/schema.rb
+++ react-testing/todo-react-app/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209214444) do
+ActiveRecord::Schema.define(version: 20151213060529) do
+
+  create_table "steps", force: :cascade do |t|
+    t.string   "title",                      null: false
+    t.string   "body"
+    t.integer  "todo_id"
+    t.boolean  "done",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "steps", ["todo_id"], name: "index_steps_on_todo_id"
 
   create_table "todos", force: :cascade do |t|
     t.string   "title",                      null: false
