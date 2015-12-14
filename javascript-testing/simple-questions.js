8014f71522f74e96cdd755fef67b16074312d6f3
Derek Chan <derekchan916@gmail.com>
2015-12-14 15:30:19 -0500

    Add fibonacci alg.

--- javascript-testing/simple-questions.js
+++ javascript-testing/simple-questions.js
@@ -32,3 +32,19 @@ function factors(num) {
   };
   return array;
 };
+
+//fibonacci
+function fibonacci(n){
+  if (n === 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    var last = fibonacci(n - 1);
+    last.push(last[last.length - 1] + last[last.length - 2]);
+
+    return last;
+  }
+}
