37f41dd6a1b9ee7f7a4929d2558d357dff76d1e1
Derek Chan <derekchan916@gmail.com>
2015-12-14 16:12:13 -0500

    Do merge sort.

--- javascript-testing/simple-questions.js
+++ javascript-testing/simple-questions.js
@@ -110,5 +110,21 @@ function bsearch(numbers, target) {
 function merge(left, right) {
   var merged = [];
 
-  while (left.length > 0 && right.length > 0)
-}
+  while (left.length > 0 && right.length > 0) {
+    var item = (left[0] < right[0]) ? left.shift() : right.shift();
+    merged.push(item);
+  };
+
+  return merged.concat(left, right);
+};
+
+function mergeSort(array) {
+  if (array.length < 2) {
+    return array;
+  } else {
+    var middle = Math.floor(array.length / 2);
+    var left = mergeSort(array.slice(0, middle));
+    var right = mergeSort(array.slice(middle));
+    return merge(left, right);
+  }
+};
