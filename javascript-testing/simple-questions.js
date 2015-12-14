d4ddd8a9c176ad01ead3c8c34b85630af177cc45
Derek Chan <derekchan916@gmail.com>
2015-12-14 15:24:04 -0500

    Add factors.

--- javascript-testing/simple-questions.js
+++ javascript-testing/simple-questions.js
@@ -16,3 +16,19 @@ for (var i=0; i < 100; i++){
     console.log(i)
   }
 }
+
+//prime factors
+
+function factors(num) {
+  var array = [];
+  for (var i=0; i <= Math.sqrt(num); i++){
+    if (num % i === 0) {
+      array.push(i);
+
+      if (num / i != i) {
+        array.push(num / i);
+      }
+    }
+  };
+  return array;
+};
