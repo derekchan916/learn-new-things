d12e3d532ea67123149b366220c2e951367f0db8
Derek Chan <derekchan916@gmail.com>
2015-12-15 13:55:54 -0500

    Add another weird JS example.

--- javascript-testing/playground.js
+++ javascript-testing/playground.js
@@ -32,3 +32,26 @@ var foo = {
 foo.hasOwnProperty('bar') //we are expecting true but we tricked it
 ({}).hasOwnProperty(foo, 'bar') //this would be true
 Object.prototype.hasOwnProperty(foo, 'bar')//same thing
+
+
+for (var i = 0; i < 10; i++) {
+  setTimeout(function() {
+    console.log(i);
+  }, 1000);
+};
+
+for (var i = 0; i < 10; i++) {
+  (function(e) {
+    setTimeout(function() {
+      console.log(e);
+    }, 1000);
+  })(i);
+};
+
+for (var i = 0; i < 10; i++) {
+  setTimeout((function(e) {
+    return function() {
+      console.log(e)
+    }
+  })(i), 1000)
+}
