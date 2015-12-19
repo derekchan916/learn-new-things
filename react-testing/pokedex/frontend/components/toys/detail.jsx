574ffc1cf432eb7e17c6e6e556b79fa58390f120
Derek Chan <derekchan916@gmail.com>
2015-12-18 19:19:48 -0800

    Finish components for toys detail.

--- react-testing/pokedex/frontend/components/toys/detail.jsx
+++ react-testing/pokedex/frontend/components/toys/detail.jsx
@@ -1,1 +1,48 @@
 var React = require('react');
+var PokemonStore = require('../../stores/pokemon.js');
+
+module.exports = React.createClass({
+  getInitialState: function() {
+    return this.getStateFromStore();
+  },
+
+  getStateFromStore: function() {
+    var pokemon = PokemonStore.find(parseInt(this.props.params.pokemonId));
+    var toy;
+    pokemon && pokemon.toys && pokemon.toys.forEach(function(t) {
+      if (t.id === parseInt(this.props.params.toyId)) { toy = t }
+    }.bind(this))
+    return {toy: toy}
+  },
+
+  _onChange: function() {
+    this.setState(this.getStateFromStore());
+  },
+
+  componentDidMount: function() {
+    this.pokemonListener = PokemonStore.addListener(this._onChange);
+  },
+
+  componentWillUnmount: function() {
+    this.pokemonListener.remove();
+  },
+
+  componentWillReceiveProps: function() {
+    this._onChange();
+  },
+
+  render: function() {
+    if(this.state.toy === undefined) { return <div></div>; }
+
+    return(
+      <div className="toy-detail-pane">
+        <div className="detail">
+          <img src={this.state.toy.image_url} />
+          {['name', 'happiness', 'price'].map(function (attr) {
+            return <p key={attr}>{attr}: {this.state.toy[attr]}</p>;
+          }.bind(this))}
+        </div>
+      </div>
+    );
+  }
+})
