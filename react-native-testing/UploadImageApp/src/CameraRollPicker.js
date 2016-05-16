3a77e1ab41e1f549ff86bdce993bc3a5b6960e67
Derek Chan <derekchan916@gmail.com>
2016-05-16 11:55:06 -0400

    Figure out why the images arent uploading.

--- react-native-testing/UploadImageApp/src/CameraRollPicker.js
+++ react-native-testing/UploadImageApp/src/CameraRollPicker.js
@@ -19,6 +19,7 @@ class CameraRollPicker extends Component {
 	constructor() {
 		super();
 		this.state = {
+			imageSource: null,
 			dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
 			imageSourceArr: [null]
 		}
@@ -63,11 +64,14 @@ class CameraRollPicker extends Component {
 				// const source = {uri: response.uri.replace('file://', ''), isStatic: true};
 				var localImageArray = this.state.imageSourceArr.slice();
 				//need to do check if the item is null, if it is then add item, if it isnt then replace.
+				// this.setState({
+				// 	imageSourceArr: localImageArray
+				// 					.slice(0, localImageArray.length -1 )
+				// 					.concat(source, localImageArray[localImageArray.length - 1])
+				//  });
 				this.setState({
-					imageSourceArr: localImageArray
-									.slice(0, localImageArray.length -1 )
-									.concat(source, localImageArray[localImageArray.length - 1])
-				 });
+					imageSource: source
+				})
 			}
 			this.setState({
 				dataSource: this.state.dataSource.cloneWithRows(this.state.imageSourceArr)
@@ -83,27 +87,48 @@ class CameraRollPicker extends Component {
 	}
 
 	render() {
-		return (
-			<View>
-				<ListView contentContainerStyle={styles.list}
-			        dataSource={this.state.dataSource}
-			        renderRow={(rowData, sectionID, rowID) => this.renderRow(rowData, sectionID, rowID)}
-				/>
-			</View>
-		)
+		var testing = true;
+
+		if (!testing) {
+			return (
+				<View>
+					<ListView contentContainerStyle={styles.list}
+						dataSource={this.state.dataSource}
+						renderRow={(rowData, sectionID, rowID) => this.renderRow(rowData, sectionID, rowID)}
+					/>
+					<TouchableOpacity onPress={() => this.selectPhotoTapped(this.state.imageSourceArr.length - 1)}>
+						<View style={[styles.image, styles.imageContainer]}>
+							<Text>+</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
+			)
+		} else {
+			return (
+				<View style={styles.container}>
+		        <TouchableOpacity onPress={() => this.selectPhotoTapped(1)}>
+		          <View style={[styles.image, styles.imageContainer, {marginBottom: 20}]}>
+		          { this.state.imageSource === null ? <Text>Select a Photo</Text> :
+		            <Image style={styles.image} source={this.state.imageSource} />
+		          }
+		          </View>
+		        </TouchableOpacity>
+		      </View>
+			)
+		}
 	}
 
 	renderRow(rowData: string, sectionID: number, rowID: number) {
 		return (
 			<View>
-				<TouchableOpacity onPress={() => this.selectPhotoTapped(parseInt(rowID))}>
-					<View style={[styles.image, styles.imageContainer]}>
-						{ rowData === null ? <Text>+</Text> :
+				{ rowData === null ? null :
+					<TouchableOpacity onPress={() => this.selectPhotoTapped(parseInt(rowID))}>
+						<View style={[styles.image, styles.imageContainer]}>
 							<Image style={styles.image} source={rowData.imageSource} />
-						}
-					</View>
-				<Text onPress={() => this.removeImage(parseInt(rowID))}>X</Text>
-				</TouchableOpacity>
+						</View>
+						<Text onPress={() => this.removeImage(parseInt(rowID))}>X</Text>
+					</TouchableOpacity>
+				}
 			</View>
 		)
 	}
