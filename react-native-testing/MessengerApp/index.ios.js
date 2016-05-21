3651af20c33dac42fc524045fda309864ffc5121
Derek Chan <derekchan916@gmail.com>
2016-05-21 18:12:12 -0400

    Added messengerApp.

---
+++ react-native-testing/MessengerApp/index.ios.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+ 'use strict';
+
+ var ReactNative = require('react-native');
+ var {
+   AppRegistry
+ } = ReactNative;
+
+
+ AppRegistry.registerComponent('GiftedMessengerExample', () => require('./src/Navigation'));
